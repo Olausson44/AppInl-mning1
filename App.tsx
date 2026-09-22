86120bbd2eb19ac7cb65b599c6e0684b94939511
Christian Olausson <olausson44@gmail.com>
2026-09-22 20:08:37 +0200

    Add click counter with useState

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
-import * as Haptics from 'expo-haptics'
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import * as Haptics from "expo-haptics";
 
 export default function App() {
+  const [buttonValue, setbuttonValue] = useState(0);
   return (
-    <View style={[styles.container, {backgroundColor: 'blue'}]}>
-      <Text>Hello, World!</Text>
-      <Pressable style={styles.button} onPress={() => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)}>
-        <Text>Klicka mig!</Text></Pressable>
+    <View style={[styles.container, { backgroundColor: "blue" }]}>
+      <Pressable
+        style={styles.button}
+        onPress={() => {
+          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
+          setbuttonValue(buttonValue + 1);
+        }}
+      >
+        <Text>Klicka mig!</Text>
+      </Pressable>
+      <Text style={styles.counter}>{buttonValue}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,16 +25,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 
-  button:{
-    backgroundColor: '#46e91e',
+  button: {
+    backgroundColor: "#46e91e",
     paddingVertical: 12,
     paddingHorizontal: 24,
     borderRadius: 9,
     marginTop: 20,
-  }
+  },
+  counter: {
+    marginTop: 20,
+    color: "#ffff",
+    fontSize: 26,
+  },
 });
