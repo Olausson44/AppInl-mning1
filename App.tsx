aaa6f695371a0f878fc6f78983e857d7b937df12
Christian Olausson <olausson44@gmail.com>
2026-09-22 19:53:29 +0200

    Add styled button and haptics

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import * as Haptics from 'expo-haptics'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={[styles.container, {backgroundColor: 'blue'}]}>
+      <Text>Hello, World!</Text>
+      <Pressable style={styles.button} onPress={() => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)}>
+        <Text>Klicka mig!</Text></Pressable>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +20,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  button:{
+    backgroundColor: '#46e91e',
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 9,
+    marginTop: 20,
+  }
 });
